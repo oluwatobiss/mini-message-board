2dd478a5c77c317d1f295f080dc10c9ded48aaa2
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2024-08-22 16:11:04 +0100

    feat: Create a route for adding new posts

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const port = 3000;
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRouter);
 
 app.listen(port, () =>
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -24,4 +24,17 @@ indexRouter.get("/", (req, res) =>
   res.render("index", { title: "Mini Messageboard", messages: messages })
 );
 
+indexRouter.get("/new", (req, res) =>
+  res.render("form", { title: "New Post", messages: messages })
+);
+
+indexRouter.post("/new", (req, res) => {
+  const user = req.body.user;
+  const text = req.body.text;
+  if (user && text) {
+    messages.push({ text, user, added: new Date() });
+    res.redirect("/");
+  }
+});
+
 module.exports = indexRouter;
