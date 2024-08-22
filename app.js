1946a17f4141bc8fadf376103a02be3d826117c4
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2024-08-22 13:44:11 +0100

    feat: Create router for the index paths

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("node:path");
+const indexRouter = require("./routes/indexRouter");
 
 const app = express();
 const port = 3000;
@@ -7,22 +8,7 @@ const port = 3000;
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-];
-
-app.get("/", (req, res) =>
-  res.render("index", { title: "Mini Messageboard", messages: messages })
-);
+app.use("/", indexRouter);
 
 app.listen(port, () =>
   console.log(`Server listening for requests at port: ${port}!`)
