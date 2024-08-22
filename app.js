9aff7ebb70fb5b2be3a058076c94523770ab69ae
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2024-08-22 19:24:01 +0100

    style: Format home page

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const postsRouter = require("./routes/postsRouter");
 const app = express();
 const port = 3000;
 
-app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+
 app.use("/", indexRouter);
 app.use("/posts", postsRouter);
 
