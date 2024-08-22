1430781cede3c7c4895b3dce9c5571b15cb0ebbf
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2024-08-22 16:48:05 +0100

    feat: Create controller file for the index routes

---
+++ controllers/indexController.js
@@ -0,0 +1,34 @@
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+  {
+    text: "Hello Devs!",
+    user: "David",
+    added: new Date(),
+  },
+];
+
+const getPosts = (req, res) =>
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+
+const getForm = (req, res) =>
+  res.render("form", { title: "New Post", messages: messages });
+
+const createPost = (req, res) => {
+  const user = req.body.user;
+  const text = req.body.text;
+  if (user && text) {
+    messages.push({ text, user, added: new Date() });
+    res.redirect("/");
+  }
+};
+
+module.exports = { getPosts, getForm, createPost };
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,40 +1,14 @@
 const { Router } = require("express");
+const {
+  getPosts,
+  getForm,
+  createPost,
+} = require("../controllers/indexController");
 
 const indexRouter = Router();
 
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-  {
-    text: "Hello Devs!",
-    user: "David",
-    added: new Date(),
-  },
-];
-
-indexRouter.get("/", (req, res) =>
-  res.render("index", { title: "Mini Messageboard", messages: messages })
-);
-
-indexRouter.get("/new", (req, res) =>
-  res.render("form", { title: "New Post", messages: messages })
-);
-
-indexRouter.post("/new", (req, res) => {
-  const user = req.body.user;
-  const text = req.body.text;
-  if (user && text) {
-    messages.push({ text, user, added: new Date() });
-    res.redirect("/");
-  }
-});
+indexRouter.get("/", getPosts);
+indexRouter.get("/new", getForm);
+indexRouter.post("/new", createPost);
 
 module.exports = indexRouter;
