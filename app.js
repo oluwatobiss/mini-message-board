d951a79902aae22b14dbbbc972f72996045125cb
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2024-08-22 12:15:37 +0100

    feat: Create an index view

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const express = require("express");
+const path = require("node:path");
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello!"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+app.get("/", (req, res) =>
+  res.render("index", { title: "Mini Messageboard", messages: messages })
+);
 
 app.listen(port, () =>
   console.log(`Server listening for requests at port: ${port}!`)
