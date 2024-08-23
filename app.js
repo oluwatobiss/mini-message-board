f004eda06f178365a13ef8de145df50ea1a78183
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2024-08-23 02:23:06 +0100

    feat: Allow altering the listening port by passing a port environment variable

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const indexRouter = require("./routes/indexRouter");
 const postsRouter = require("./routes/postsRouter");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
