b77257847ece295d932bb7ff708a55261e86b85a
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2024-09-04 23:16:44 +0100

    refactor: Use connection string

--- db/queries.js
+++ db/queries.js
@@ -1,5 +1,5 @@
 const { Pool } = require("pg");
-const pool = new Pool();
+const pool = new Pool({ connectionString: process.env.DB_URI });
 const createTableSQL = `
 CREATE TABLE IF NOT EXISTS messages (
   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
