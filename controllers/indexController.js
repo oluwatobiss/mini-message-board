23829cada3228de4298aab595d31a7b9d4bdfe81
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2024-09-04 18:53:40 +0100

    feat: Validate users input

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,4 +1,12 @@
+const { body, validationResult } = require("express-validator");
 const db = require("../db/queries");
+
+const messageFormValidator = body("user")
+  .trim()
+  .isAlpha()
+  .withMessage("Username must contain letters only.")
+  .isLength({ min: 4, max: 30 })
+  .withMessage("Username must be between 4 to 30 characters.");
 
 async function getAllUsersPosts(req, res) {
   const messages = await db.getAllMessages();
@@ -7,11 +15,21 @@ async function getAllUsersPosts(req, res) {
 
 const getForm = (req, res) => res.render("form", { title: "New Post" });
 
-async function createPost(req, res) {
-  const user = req.body.user;
-  const text = req.body.text;
-  await db.insertMessage([user, text, new Date()]);
-  res.redirect("/");
-}
+const createPost = [
+  messageFormValidator,
+  async (req, res) => {
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      return res.status(400).render("form", {
+        title: "New Post",
+        errors: result.array(),
+      });
+    }
+    const user = req.body.user;
+    const text = req.body.text;
+    await db.insertMessage([user, text, new Date()]);
+    res.redirect("/");
+  },
+];
 
 module.exports = { getAllUsersPosts, getForm, createPost };
